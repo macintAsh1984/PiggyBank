8e28d62b20c403334d500fe94cd5fed5900f7217
macintAsh1984 <amvaldez@ucdavis.edu>
2024-01-12 15:15:08 -0800

    Added starter files.

---
+++ PiggyBank/PiggyBank/PiggyBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiggyBankApp.swift
+//  PiggyBank
+//
+//  Created by Ashley Valdez on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PiggyBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
