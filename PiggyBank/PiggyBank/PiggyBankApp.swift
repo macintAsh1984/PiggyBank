a52af3a78d85653f6216fdac3709b81dfca6b78e
macintAsh1984 <amvaldez@ucdavis.edu>
2024-01-16 10:03:38 -0800

    [Issue #5] Created PiggyBank splash screen.

--- PiggyBank/PiggyBank/PiggyBankApp.swift
+++ PiggyBank/PiggyBank/PiggyBankApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct PiggyBankApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreen()
         }
     }
 }
---
+++ PiggyBank/PiggyBank/SplashScreen.swift
@@ -0,0 +1,42 @@
+//
+//  SplashScreen.swift
+//  PiggyBank
+//
+//  Created by Ashley Valdez on 1/16/24.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    @State var splashScreenIsActive = false
+    
+    var body: some View {
+        if splashScreenIsActive {
+            ContentView()
+        } else {
+            VStack {
+                VStack {
+                    Image("PiggyBank Icon")
+                    Text("PiggyBank")
+                        .font(.custom("CandyBeans", size: 50.0, relativeTo: .title))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                }
+            }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity, maxHeight: .infinity/*@END_MENU_TOKEN@*/)
+            .background(Color(appBackgroundColor))
+            .preferredColorScheme(.light)
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.splashScreenIsActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
