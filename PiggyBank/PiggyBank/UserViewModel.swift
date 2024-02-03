a31ab7d52df314797765ddebfa94bc7f1db87549
macintAsh1984 <amvaldez@ucdavis.edu>
2024-02-03 14:02:59 -0800

    [Issue #10] Created a user view model to publish user account info changes to different views in PiggyBank.

---
+++ PiggyBank/PiggyBank/UserViewModel.swift
@@ -0,0 +1,70 @@
+//
+//  UserViewModel.swift
+//  PiggyBank
+//
+//  Created by Ashley Valdez on 1/29/24.
+//
+
+import SwiftUI
+
+class PiggyBankUser: ObservableObject {
+    @Published var name: String = ""
+    @Published var phoneNumber: String = ""
+    @Published var authToken: String?
+    @Published var accounts: [Account] = [Account]()
+    
+    
+    let noAuthTokenError = ApiError(errorCode: "no_auth_token_error", message: "No auth token has been set for this user")
+    let setAuthTokenError = ApiError(errorCode: "set_auth_token_error", message: "Failed to set user auth token")
+    let setUserNameError = ApiError(errorCode: "set_user_name_error", message: "Failed to set username")
+    
+    func saveAuthToken(e164phoneNumber: String, code: String) async throws {
+        guard let checkVerificationResponse = try? await Api.shared.checkVerificationToken(e164PhoneNumber: e164phoneNumber, code: code) else { throw setAuthTokenError }
+        UserDefaults.standard.setValue(checkVerificationResponse.authToken, forKey: "authToken")
+    }
+    
+    func setUserName(username: String) {
+        self.name = username
+    }
+    
+    func setPhoneNumber(phoneNumber: String) {
+        self.phoneNumber = phoneNumber
+        UserDefaults.standard.setValue(self.phoneNumber, forKey: "phoneNumber")
+    }
+    
+    func getPhoneNumber() -> String {
+        return self.phoneNumber
+    }
+    
+    func saveUserName(name: String, authToken: String) async throws {
+        guard let username = try? await Api.shared.setUserName(authToken: authToken, name: name) else { throw setUserNameError }
+        UserDefaults.standard.setValue(username.user.name, forKey: "name")
+    }
+    
+    func loadUserName() {
+        if let username =  UserDefaults.standard.string(forKey: "name") {
+            self.name = username
+        } else {
+            print("hi")
+        }
+    }
+    
+    func loadPhoneNumber() {
+        if let phoneNumber =  UserDefaults.standard.string(forKey: "phoneNumber") {
+            self.phoneNumber = phoneNumber
+        } else {
+            print("hi")
+        }
+    }
+    
+
+    func loadUser() {
+        if let authToken =  UserDefaults.standard.string(forKey: "authToken") {
+            self.authToken = authToken
+        } else {
+            print("hi")
+        }
+        
+    }
+    
+}
